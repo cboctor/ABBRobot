430ce9668ab89cb4619857ae3cc429e19b1eb0b2
Christopher <christopher.boctor@student.uts.edu.au>
2013-05-20 17:24:03 +1000

    Initial Commit

---
+++ ABBRobot/ABBRobot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABB.Robotics.RobotStudio.Stations;
+
+namespace ABBRobot
+{
+    public class ABBRobot
+    {
+    }
+}
